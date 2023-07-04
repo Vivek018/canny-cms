d7f076c6fc2f6d78b357050a44c59a05972bb2cc
Vivek <vc1262461@gmail.com>
2023-07-04 19:23:58 +0530

    Changed Navbar style

--- src/app/(frontend)/[lang]/_components/navbar/DesktopNav.tsx
+++ src/app/(frontend)/[lang]/_components/navbar/DesktopNav.tsx
@@ -4,6 +4,7 @@ import { Lang, Page } from "@/types";
 import { cn } from "@/utils/cn";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 type Props = {
   items: Page[];
@@ -12,6 +13,7 @@ type Props = {
 };
 
 export function DesktopNav({ items, className, lang }: Props) {
+
   return (
     <div className={cn("hidden md:flex mx-auto text-sm", className)}>
       {items.map((item) => (
@@ -28,9 +30,9 @@ const DesktopNavItem = ({ item, lang }: { item: Page; lang: Lang }) => {
     <Link
       href={url}
       className={cn(
-        "flex-auto border-[1px] border-neutral-shadow shadow-sm border-r-0 last:border-r-2 px-4 py-2 first:rounded-l-full last:rounded-r-full cursor-pointer hover:bg-neutral-primary transition duration-200",
+        "flex-auto md:px-3 md:mx-2 lg:px-4 lg:mx-4 py-2 cursor-pointer transition duration-200 hover:text-primary",
         pathname === url &&
-          "bg-neutral-shadow hover:bg-neutral-shadow text-primary shadow-inner cursor-auto"
+          " bg-primary hover:text-white text-white rounded-md shadow-md cursor-auto"
       )}
     >
       <nav>{item.title}</nav>
